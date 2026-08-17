8192ee60fe2f8474bf0eb03c5c8c1f4b7b8a2770
SANTHOSH S V <santhoshsv0927@gmail.com>
2026-08-17 13:05:43 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class',
+    content: [
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    DEFAULT: '#C21807',
+                    dark: '#8B1005',
+                },
+            },
+        },
+    },
+    plugins: [],
+}
